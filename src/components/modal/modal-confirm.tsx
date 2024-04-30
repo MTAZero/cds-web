9190b49cf435406f09629f421c4c7752a8a9164b
MTAZero <buithuy3996@gmail.com>
2024-04-30 16:33:20 +0700

    feat: update transition show modal

--- src/components/modal/modal-confirm.tsx
+++ src/components/modal/modal-confirm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Modal } from "@mui/material";
+import { Box, Button, Grow, Modal } from "@mui/material";
 import React from "react";
 
 import * as styles from "./modal-confirm.styles";
@@ -27,21 +27,23 @@ export const ModalConfirm: React.FC<ModalConfirmProps> = ({
 }) => {
   return (
     <Modal open={visible} onClose={onClose} style={styles.modalStyle}>
-      <Box sx={styles.containerStyle}>
-        <Box sx={styles.topModalStyle}>
-          <Box sx={styles.titleStyle}>{title}</Box>
-          <FaTimes />
+      <Grow in={visible}>
+        <Box sx={styles.containerStyle}>
+          <Box sx={styles.topModalStyle}>
+            <Box sx={styles.titleStyle}>{title}</Box>
+            <FaTimes onClick={onClose} style={{ cursor: "pointer" }} />
+          </Box>
+          <Box sx={styles.contentStyle}>{message}</Box>
+          <Box sx={styles.buttonPanelStyle}>
+            <Button onClick={onConfirm} sx={styles.buttonOKStyle}>
+              {okText}
+            </Button>
+            <Button onClick={onCancel} sx={styles.buttonCancelStyle}>
+              {cancelText}
+            </Button>
+          </Box>
         </Box>
-        <Box sx={styles.contentStyle}>{message}</Box>
-        <Box sx={styles.buttonPanelStyle}>
-          <Button onClick={onConfirm} sx={styles.buttonOKStyle}>
-            {okText}
-          </Button>
-          <Button onClick={onCancel} sx={styles.buttonCancelStyle}>
-            {cancelText}
-          </Button>
-        </Box>
-      </Box>
+      </Grow>
     </Modal>
   );
 };
